7ba8b8fc20a82ec2b3aac0e4bd1045ae440f3349
kumarapusrivarsha <134908628+kumarapusrivarsha@users.noreply.github.com>
2026-05-26 13:33:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const line = document.getElementById("line");
+let angle = 0;
+
+setInterval(function () {
+  angle += 2;
+  line.style.transform = `rotate(${angle}deg)`;
+}, 20);
